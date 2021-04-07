59a3655be0382b93b84f874aee44e081e43b316c
sssofi0101 <sssofi0101@gmail.com>
2021-04-07 20:12:32 +0300

    Добавила интерфейсы первых двух страниц

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class Container(BoxLayout):
+class HelloScreen(Screen):
+    pass
+
+
+class CountTeamsScreen(Screen):
     pass
 
 
 class MyApp(App):
     def build(self):
-        return Container()
+        sm = ScreenManager()
+        sm.add_widget(HelloScreen(name='firstscreen'))
+        sm.add_widget(CountTeamsScreen(name='secondscreen'))
+        return sm
 
 
 if __name__ == '__main__':
